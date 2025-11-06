ce1dff23b9d2f54b7878da92406fed62122ca29f
Aswathi <aswathiaj23@gmail.com>
2025-11-06 15:44:10 +0530

    modified

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    // This will define global variables that are replaced at build time
-    'process.env': {}
+  preview: {
+    allowedHosts: true  // This allows all hosts
+  },
+  server: {
+    allowedHosts: true  // This allows all hosts
   }
 })
