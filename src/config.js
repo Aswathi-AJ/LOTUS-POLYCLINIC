37ea393077b918bf199d97b23f75f26801bfcee5
Aswathi <aswathiaj23@gmail.com>
2025-11-07 18:52:36 +0530

    modified

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 const config = {
   CLINIC_EMAIL: import.meta.env.VITE_CLINIC_EMAIL || 'aswathiaj23@gmail.com',
-  CLINIC_PHONE: import.meta.env.VITE_CLINIC_PHONE || '+91 9876543210',
+  CLINIC_PHONE: import.meta.env.VITE_CLINIC_PHONE || '+91 98403 98908',
   EMAILJS_PUBLIC_KEY: import.meta.env.VITE_EMAILJS_PUBLIC_KEY || '7hytwlaS_99jy7O5o',
   EMAILJS_SERVICE_ID: import.meta.env.VITE_EMAILJS_SERVICE_ID || 'service_u0dyx6e',
   EMAILJS_TEMPLATE_ID: import.meta.env.VITE_EMAILJS_TEMPLATE_ID || 'template_6rr7ecb',
